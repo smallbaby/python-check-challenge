bf6683cb56860eaeb4bdedd49fe43cf7440f7fdd
kai.zhang01 <kai.zhang01@fengjr.com>
2017-10-08 21:01:08 +0800

    update issues#3

--- random_numbers.py
+++ random_numbers.py
@@ -1,6 +1,16 @@
 # -*- coding:utf8 -*-
 import random
 
-# first
-numbers = [random.randint(0,10000) for _ in range(1000)]
+# 第一步生成10个随机数
+numbers = [random.randint(0,10000) for _ in range(10)]
 print numbers
+
+# output
+'''
+
+[7584, 5108, 321, 4713, 875, 9525, 3631, 1007, 5784, 4972]
+
+'''
+
+# TODO : 不重复的随机数
+
