fd791828a4778cfe5d2b2d866b2e716a8efe73a2
Jason zhang <zhangkai081@gmail.com>
2017-10-28 10:05:03 -0500

    Update test_RockBomb.py

--- test_RockBomb.py
+++ test_RockBomb.py
@@ -7,13 +7,14 @@ import urllib2
 # read data from url
 # 数据源已经失效....通过google找到了所有的数据源，发现不是失效，而是必须墙....
 # 求得输入数据有多少行，每行有多少列
-target_url = ('https://archive.ics.uci.edu/m1/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data')
+#target_url = ('https://archive.ics.uci.edu/m1/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data')
 
-data = urllib2.urlopen(target_url)
+# data = urllib2.urlopen(target_url)
+file = open('sonar.all-data', 'r')
 
 xList = []
 labels = []
-for line in data:
+for line in file.readlines():
     row = line.strip().split(',')
     xList.append(row)
 
